b71066755344ac3933cdd777d701c92ae1d7aaea
ritikk20 <144711029+ritikk20@users.noreply.github.com>
2024-04-09 00:37:04 +0530

    Update 1

--- Sudokugame.cpp
+++ Sudokugame.cpp
@@ -3,12 +3,119 @@
 #include <vector>
 using namespace std;
 void board(vector<vector<int>> &mat)
+{
+
+    printf("\t");
+    for (int i = 0; i < 10; i++)
+    {
+
+        for (int j = 0; j <= 9; j++)
+        {
+            if (j == 0)
+            {
+                printf("\x1b[30m"
+                       " %2d "
+                       "\x1b[0m"
+                       "\x1b[34m",
+                       i);
+                cout << "  ";
+                continue;
+            }
+            if (i == 0)
+            {
+                printf("\x1b[30m"
+                       "%d "
+                       "\x1b[0m"
+                       "\x1b[34m",
+                       j);
+                if (j == 3 || j == 6)
+                    printf("   ");
+                if (j == 9)
+                    cout << endl;
+                continue;
+            }
+            if (mat[i - 1][j - 1])
+            {
+                if (mat[i - 1][j - 1] < 0)
+                    printf("\x1b[0m"
+                           "%d "
+                           "\x1b[34m",
+                           -mat[i - 1][j - 1]);
+                else
+                    printf("\x1b[33m"
+                           "%d "
+                           "\x1b[34m",
+                           mat[i - 1][j - 1]);
+            }
+            else
+                cout << mat[i - 1][j - 1] << " ";
+            if (j == 3 || j == 6)
+                printf(" | ");
+        }
+        if (i == 3 || i == 6)
+            printf("\n\t     ------------------------");
+        printf("\n\t");
+    }
+    printf("\x1b[0m");
 }
 
 bool Iscompleted(vector<vector<int>> &mat)
+{
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            if (mat[i][j] == 0)
+                return false;
+    return true;
 }
 
 bool Isvalid(vector<vector<int>> &mat)
+{
+
+    vector<vector<bool>> square(3, vector<bool>(10, false));
+    for (int i = 0; i < 9; i++)
+    {
+        vector<bool> col(10, false);
+        vector<bool> row(10, false);
+        for (int j = 0; j < 9; j++)
+        {
+            if (col[mat[i][j]] || row[mat[j][i]] || square[j / 3][mat[i][j]])
+            { // duplicate data
+                return false;
+            }
+            else
+            {
+                col[mat[i][j]] = true;
+                row[mat[j][i]] = true;
+                square[j / 3][mat[i][j]] = true;
+            }
+        }
+        for (int j = 1; j < 10; j++)
+        {
+
+            if (!col[j] || !row[j])
+            { // any missing
+                return false;
+            }
+        }
+        if (i == 2 || i == 5)
+        {
+            for (int p = 0; p < 3; p++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    if (!square[p][j])
+                    { // any missing
+                        return false;
+                    }
+                    else
+                    {
+                        square[p][j] = false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
 }
 
 int main()
